73203946c28297f041c12abf7f3be7825d37d394
Samuel de Oliveira Silva <samuelsilvapd61@gmail.com>
2025-05-21 20:19:53 -0300

    Versão inicial

---
+++ src/test/java/com/samuel/l2challenge/Empacotamento/EmpacotamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.l2challenge.Empacotamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpacotamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
